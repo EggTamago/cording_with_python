82e750e8945b75bd13b5678b6d718d8ea2bede00
EggTamago <keiichiro.tany@gmail.com>
2021-07-03 14:02:23 +0000

    add post & BaseModel

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,21 +1,28 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+
+from typing import Optional
 
 from calc import plus 
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"data": "blog list"}
+@app.get("/blog")
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 pblished blogs
+    if published:
+        return {"data": f'{limit} published blog from the db'}
+    else:
+        return {"data": f'{limit} blog from the db'}
 
 @app.get("/blog/unpublished")
 def unpublished():
     return {'data': 'unpubulished'}
 
 @app.get('/blog/{id}')
-def show():
+def show(id: int):
     # fetch blog with id = id
     return {'data': id }
 
@@ -32,10 +39,16 @@ def comments(id: int):
     # fetch commetns of blog with id = id
     return {'data': {'10', '20'} }
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    item_id = plus(item_id, item_id)
-    return {"item_id": item_id, "q": q}
+
+# BaseModel扱うと引数簡略化できる
+class Blog(BaseModel):
+    title: str
+    body: str
+    published_at: Optional[bool]
+
+@app.post('/blog')
+def create_blog(blog: Blog):
+    return { "data" : f"blog is created with {blog.title}"}
 
 
 if __name__ == "__main__":
