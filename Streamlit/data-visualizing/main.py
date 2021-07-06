8e4b691fb378a0f3ba97de4bd566f41a3f206a77
EggTamago <keiichiro.tany@gmail.com>
2021-07-06 23:26:41 +0000

    add show graph

--- Streamlit/data-visualizing/main.py
+++ Streamlit/data-visualizing/main.py
@@ -7,9 +7,8 @@ from PIL import Image
 # Page Title
 ######################
 
-image = Image.open('photo.jpg')
-
-st.image(image, use_column_width=True)
+st.sidebar.header('User Input Features')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 
 st.write("""
 # Data visualize App
@@ -17,7 +16,27 @@ This app show your csv data on table or graph.
 ***
 """)
 
+st.write("""
+# Show your data on Table
+""")
+
 uploaded_file = st.file_uploader("Choose an your csv file...", type = 'csv')
 if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
     st.write(pd.DataFrame(data))
+
+
+st.write("""
+***
+""")
+
+st.write("""
+# Show your data on graph
+""")
+if st.checkbox('Show DataFrame by chart'):
+    if uploaded_file is not None:
+        chart_df = pd.DataFrame(data)
+        st.line_chart(chart_df)
+if st.checkbox('Show DataFrame by histgram'):
+    if uploaded_file is not None:
+        st.altair_chart(data)
