33ca627f73a98b05eccdb71a5d4d857733cad944
EggTamago <keiichiro.tany@gmail.com>
2021-07-13 14:59:46 +0000

    add scraping

--- scraping/main.py
+++ scraping/main.py
@@ -1,6 +1,9 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
+import tqdm
+
+
 #
 # S1
 #
@@ -12,14 +15,46 @@ s1_url = 'https://s1s1s1.com/actress/'
 # seach format
 form = ['a', 'ka', 'sa', 'ta', 'na', 'ha', 'ma', 'ya', 'ra', 'wa']
 
-for target in form:
-    target_url = s1_url + target + '/'
-    print(target_url)
+for target in tqdm.tqdm(form):
+    target_url = s1_url + target
 
-    # a~waまで順番にwebページの取得し、女優名リストを作成
-    response = request.urlopen(s1_url)
-    soup = BeautifulSoup(response)
-    response.close()
+    for i in range(1,9):
+        target_url = target_url + f'?page={i}'
+        
+        # a~waまで順番にwebページの取得
+        response = request.urlopen(target_url)
+        soup = BeautifulSoup(response, "lxml")
+
+        # 女優名を取得
+        temp = []
+        temp = soup.find_all('p', class_='name')
+
+        for j in range(len(temp)):
+            try:
+                s1_list.append(temp[j].text)
+            except Exception as e:
+                pass
+
+        response.close()
+
+        # target_url初期化
+        target_url = s1_url + target
+
+s1_list = list(set(s1_list))
+print(s1_list)
+
+
+    # # 女優名を取得
+    # s1_list = soup.find_all('p', class_='name')
+    # response.close()
+
+    # print(s1_list)
+    # print()
+    # print(s1_list[0])
+    # print()
+    # print(s1_list[0].text)
+    # print()
+    # print(len(s1_list))
 
     
 
