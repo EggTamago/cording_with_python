c7923f1ee5da6b22b7bff3f6c369c6d167cbede2
Tamago <keiichiro.tany@gmail.com>
2021-07-15 15:28:09 +0000

    add roulet

--- scraping/daily/src/components/App.js
+++ scraping/daily/src/components/App.js
@@ -1,21 +1,43 @@
-import React from 'react'
+import { React, useState } from 'react'
 import { Button, Grid } from '@material-ui/core';
+import { Wheel } from 'react-custom-roulette'
 
 import '../css/App.css';
 
 const App = () => {
 
-  const who = () => {
-    console.log("click this button")
+  const data = [
+    { option: '💕', style: { backgroundColor: 'green', textColor: 'black' } },
+    { option: '🤩', style: { backgroundColor: 'white' } },
+    { option: '💛' },
+  ]
+
+  const [mustSpin, setMustSpin] = useState(false);
+
+  const handleSpinClick = () => {
+      setMustSpin(true)
   }
 
   return (
-    <Grid container alignItems="center" justifyContent="center">
+    <Grid container direction="column" alignItems="center" justifyContent="center">
       <div>
         <h1>Today's choice!</h1>
       </div>
 
-      <Button onClick={() => who()} variant="contained" color="primary">push me</Button>
+      <Wheel
+      mustStartSpinning={mustSpin}
+      prizeNumber={3}
+      data={data}
+      backgroundColors={['#3e3e3e', '#df3428']}
+      textColors={['#ffffff']}
+
+      onStopSpinning={() => {
+        setMustSpin(false)
+      }}
+    />
+
+    <Button onClick={() =>handleSpinClick()} variant="contained" color="primary">push me</Button>
+
 
     </Grid>
   )
