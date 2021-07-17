2e91b1b5628c73df8a47c3912731b7d6671b9b0c
Tamago <keiichiro.tany@gmail.com>
2021-07-17 06:45:18 +0000

    add push function with axios

---
+++ scraping/front-side/src/components/App.js
@@ -0,0 +1,34 @@
+import { React, useState } from 'react'
+import { Button, Grid } from '@material-ui/core';
+
+import axios from 'axios'
+
+import '../css/App.css';
+
+const App = () => {
+
+  const [name, setName] = useState('')
+
+  const server = 'http://localhost:4040/';
+  const fastapi = () => {
+      console.log('start http communicate')
+      axios.get(server)
+        .then(res => setName(res.data.message))
+        .catch(console.error)
+  }
+
+  return (
+    <Grid container direction="column" alignItems="center" justifyContent="center">
+      <div>
+        <h1>Today's choice!</h1>
+      </div>
+
+    <Button onClick={() =>fastapi()} variant="contained" color="primary">push me</Button>
+
+    <p>Today is ...  {name} </p>
+
+    </Grid>
+  )
+}
+
+export default App;
