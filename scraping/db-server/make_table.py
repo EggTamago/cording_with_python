2817ce2bea239865d8113baa733d184cd4f6a876
Tamago <keiichiro.tany@gmail.com>
2021-07-18 14:35:48 +0000

    add db

---
+++ scraping/db-server/make_table.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+#establishing the connection
+conn = psycopg2.connect(database="postgres",
+                        user='admin',
+                        password='password',
+                        host='192.168.30.1',
+                        port= '5432')
+conn.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Preparing query to create a database
+sql = '''CREATE table mydb''';
+
+#Creating a database
+cursor.execute(sql)
+print("Database created successfully........")
+
+#Closing the connection
+conn.close()
