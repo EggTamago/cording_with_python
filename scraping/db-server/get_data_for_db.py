ac6e806c89aad8303acc49e9558d6e2234732e23
Tamago <keiichiro.tany@gmail.com>
2021-08-08 15:34:48 +0000

    add function

--- scraping/db-server/get_data_for_db.py
+++ scraping/db-server/get_data_for_db.py
@@ -4,68 +4,50 @@ import time
 
 import tqdm
 
+# =====================
+# return actor_array []
+# =====================
 
-#
-# S1
-#
+def get_actor_list(url, form):
 
-s1_list = []
+    return_list = []
 
-s1_url = 'https://s1s1s1.com/actress/'
+    for target in tqdm.tqdm(form):
+        target_url = url + target
 
-# seach format
-form = ['a', 'ka', 'sa', 'ta', 'na', 'ha', 'ma', 'ya', 'ra', 'wa']
+        for i in range(1,9):
+            target_url = target_url + f'?page={i}'
+            
+            # a~waまで順番にwebページの取得
+            response = request.urlopen(target_url)
+            soup = BeautifulSoup(response, "lxml")
 
-for target in tqdm.tqdm(form):
-    target_url = s1_url + target
+            # 女優名を取得
+            actor = []
+            actor = soup.find_all('p', class_='name')
 
-    for i in range(1,9):
-        target_url = target_url + f'?page={i}'
-        
-        # a~waまで順番にwebページの取得
-        response = request.urlopen(target_url)
-        soup = BeautifulSoup(response, "lxml")
+            for j in range(len(actor)):
+                try:
+                    return_list.append(actor[j].text)
+                except Exception as e:
+                    pass
 
-        # 女優名を取得
-        temp = []
-        temp = soup.find_all('p', class_='name')
+            # target_url初期化
+            target_url = url + target
 
-        for j in range(len(temp)):
-            try:
-                s1_list.append(temp[j].text)
-            except Exception as e:
-                pass
-
-        response.close()
-
-        # target_url初期化
-        target_url = s1_url + target
-        time.sleep(5)
-
-s1_list = list(set(s1_list))
-print(s1_list)
-
-
-    # # 女優名を取得
-    # s1_list = soup.find_all('p', class_='name')
-    # response.close()
-
-    # print(s1_list)
-    # print()
-    # print(s1_list[0])
-    # print()
-    # print(s1_list[0].text)
-    # print()
-    # print(len(s1_list))
-
+    # requestをclose
+    response.close()
     
+    # 重複を削除
+    return_list = list(set(return_list))
+    return return_list
 
 #
 # kawaii
 #
 
 # format: a, ka...
-kawaii_url = 'https://www.kawaiikawaii.jp/actress/list/'
+# kawaii_url = 'https://www.kawaiikawaii.jp/actress/list/'
 
 
 
@@ -75,7 +57,7 @@ kawaii_url = 'https://www.kawaiikawaii.jp/actress/list/'
 #
 
 # format: a, i, u...
-moodyz_url = 'https://www.moodyz.com/actress/list/'
+# moodyz_url = 'https://www.moodyz.com/actress/list/'
 
 
 
@@ -85,4 +67,4 @@ moodyz_url = 'https://www.moodyz.com/actress/list/'
 #
 
 # format: a, ka...
-ebody_url = 'https://www.av-e-body.com/actress/list/'
+# ebody_url = 'https://www.av-e-body.com/actress/list/'
--- scraping/db-server/send_data_to_db.py
+++ scraping/db-server/send_data_to_db.py
@@ -1,5 +1,15 @@
 import psycopg2
 
+from get_data_for_db import get_actor_list
+
+s1 = 'https://s1s1s1.com/actress/'
+form = ['a', 'ka', 'sa', 'ta', 'na', 'ha', 'ma', 'ya', 'ra', 'wa']
+
+test = get_actor_list(s1, form)
+print(test)
+
+
+
 #establishing the connection
 conn = psycopg2.connect(database="mydb",
                         user='admin',
@@ -15,8 +25,8 @@ cursor = conn.cursor()
 # db名に「user」は不可
 sql = '''
         INSERT INTO users (user_id, first_name, last_name, age)
-        VALUES (0, 'tamago', 'egg', 200);
-     '''
+        VALUES (3, 'tamago', 'egg', 200);
+      '''
 
 #Creating a database
 cursor.execute(sql)
