3c4ff1d436b83903b92f1c48dce21f5843282ca4
Tamago <keiichiro.tany@gmail.com>
2021-08-09 14:23:51 +0000

    send data complete

---
+++ scraping/db-server/delete_data.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python3
+
+# ================
+# delete data from db
+# ================
+
+import psycopg2
+
+def delete_table():
+
+    #establishing the connection
+    conn = psycopg2.connect(database="mydb",
+                            user='admin',
+                            password='password',
+                            host='192.168.30.1',
+                            port= '5432')
+    conn.autocommit = True
+
+    #Creating a cursor object using the cursor() method
+    cursor = conn.cursor()
+
+    #Preparing query to create a database
+    # db名に「user」は不可
+    sql = '''DELETE FROM actors;'''
+
+    #Creating a database
+    cursor.execute(sql)
+    print("Database created successfully........")
+
+    #Closing the connection
+    conn.close()
+
+
+def main():
+    delete_table()
+
+if __name__=='__main__':
+    main()
+
--- scraping/db-server/send_data.py
+++ scraping/db-server/send_data.py
@@ -6,23 +6,22 @@
 
 import psycopg2
 
-from get_data_for_db import get_actor_list
-
+from get_data import get_actor_list
 
 # s1 info
 s1_url = 'https://s1s1s1.com/actress/'
 form = ['a', 'ka', 'sa', 'ta', 'na', 'ha', 'ma', 'ya', 'ra', 'wa']
-#s1_list = get_actor_list(s1_url, form)
+s1_list = get_actor_list(s1_url, form)
 
-def make_insert_data(label, actor_info):
+def insert_data(label, actor_info):
   
-    insert_data = []
+    data = []
 
     for i in range(len(actor_info)):
         tuple_list = (i, label, actor_info[i])
-        insert_data.append(tuple_list)
+        data.append(tuple_list)
 
-    return insert_data
+    return data
 
 
 def send_db(actor_info):
@@ -38,19 +37,16 @@ def send_db(actor_info):
     #Creating a cursor object using the cursor() method
     cursor = conn.cursor()
 
-    # 女優の数だけデータ投入
-    for i in range(len(actor_info)):
+    #Preparing query to create a database
+    # db名に「user」は不可
+    sql = '''
+            INSERT INTO actors (user_id, label, actor_name)
+            VALUES (%s, %s, %s);
+            '''
 
-        #Preparing query to create a database
-        # db名に「user」は不可
-        sql = '''
-                INSERT INTO actors (user_id, label, actor_name)
-                VALUES (%s, %s, %s);
-              '''
-
-        #Creating a database
-        cursor.execute(sql, data)
-        print("Database created successfully........")
+    #Creating a database
+    cursor.executemany(sql, actor_info)
+    print("Database created successfully........")
 
     #Closing the connection
     conn.close()
@@ -58,10 +54,9 @@ def send_db(actor_info):
 
 def main():
     
-    get_list = make_insert_data('s1', form)
-    send_db(get_list)
+    actor_info = insert_data('s1', s1_list)
+    send_db(actor_info)
 
 if __name__ == '__main__':
-
     main()
      
