b743588f4a0c8b6a2fd0bd265840b00d61c58144
Tamago <keiichiro.tany@gmail.com>
2021-10-20 11:28:10 +0000

    add style

--- work-time/frontend/src/App.js
+++ work-time/frontend/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid';
 import './App.css'
 
 import Header from './components/Header'
-import Checkbx from './components/Checkbox'
+import Checkbox from './components/Checkbox'
 import TimeLineChart from './components/TimeLineChart'
-
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Checkbx />
-      <TimeLineChart />
-    </div>
+    <Box sx={{ width: '100%' }}>
+      <Grid container rowSpacing={10} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={12}>
+          <Header />
+        </Grid>
+        <Grid item xs={2}>
+          <Checkbox />
+        </Grid>
+        <Grid item xs={10}>
+          <TimeLineChart />
+        </Grid>
+      </Grid>
+    </Box>
+
   )
 }
 
 export default App;
+
