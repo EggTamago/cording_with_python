353469acfa9704f97fcc29bbd53efaccf0c95c7f
EggTamago <keiichiro.tany@gmail.com>
2021-11-25 14:20:11 +0000

    first commit

---
+++ where-are-you/frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Camera from "./components/Camera";
+
+const App = () => {
+  return (
+    <Camera />
+  )
+}
+
+export default App;
---
+++ where-are-you/frontend/src/components/Camera.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Webcam from "react-webcam"
+
+const Camera = () => {
+    return (
+        <div>
+            <Webcam />
+        </div>
+    )
+}
+
+export default Camera
